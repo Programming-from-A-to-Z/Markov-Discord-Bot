70d2285d1591413b51e9b49ce68f02779d17fe98
Daniel Shiffman <daniel.shiffman@gmail.com>
2024-10-22 21:07:06 -0400

    update for 2024 and temperature

--- commands/thesis.js
+++ commands/thesis.js
@@ -12,10 +12,16 @@ const agent = new https.Agent({
   rejectUnauthorized: false,
 });
 
-const titleMarkov = new MarkovGenerator(6, 40);
-const elevatorMarkov = new MarkovGenerator(6, 1000);
+const titleMarkov = new MarkovGenerator(4, 40);
+const elevatorMarkov = new MarkovGenerator(5, 1000);
 
 async function loadData() {
+  if (fs.existsSync('titleMarkov.json') && fs.existsSync('elevatorMarkov.json')) {
+    titleMarkov.fromJSON(JSON.parse(fs.readFileSync('titleMarkov.json', 'utf8')));
+    elevatorMarkov.fromJSON(JSON.parse(fs.readFileSync('elevatorMarkov.json', 'utf8')));
+    return;
+  }
+
   // Load venues.json
   const data = JSON.parse(fs.readFileSync('./venues.json', 'utf8'));
   for (let venue of data.venues) {
@@ -26,6 +32,9 @@ async function loadData() {
     process(response.data);
   }
   console.log('finished loading data');
+
+  fs.writeFileSync('titleMarkov.json', JSON.stringify(titleMarkov));
+  fs.writeFileSync('elevatorMarkov.json', JSON.stringify(elevatorMarkov));
 }
 
 function process(data) {
@@ -49,11 +58,18 @@ loadData();
 
 export const data = new SlashCommandBuilder()
   .setName('thesis')
-  .setDescription('Generate a thesis idea.');
+  .setDescription('Generate a thesis idea.')
+  .addNumberOption((option) =>
+    option
+      .setName('temperature')
+      .setDescription('Temperature for the Markov generator')
+      .setRequired(false)
+  );
 
 export async function execute(interaction) {
-  let title = titleMarkov.generate();
-  let description = elevatorMarkov.generate();
+  const temperature = interaction.options.getNumber('temperature') || 1.0;
+  let title = titleMarkov.generate(temperature);
+  let description = elevatorMarkov.generate(temperature);
 
   await interaction.reply({
     content: '**Thesis Idea**',
--- markov.js
+++ markov.js
@@ -1,13 +1,48 @@
-// A2Z F23
+// A2Z F24
 // Daniel Shiffman
-// https://github.com/Programming-from-A-to-Z/A2Z-F23
+// https://github.com/Programming-from-A-to-Z/A2Z-F24
 
 // This is based on Allison Parrish's great RWET examples
 // https://github.com/aparrish/rwet-examples
 
-export function random(arr) {
-  const r = Math.random() * arr.length;
-  return arr[Math.floor(r)];
+export function random(val) {
+  if (Array.isArray(val)) {
+    const r = Math.random() * val.length;
+    return val[Math.floor(r)];
+  } else if (typeof val === 'number') {
+    return Math.random() * val;
+  }
+}
+
+// A weighted random selection function
+export function weightedSelection(choices, temperature = 1.0) {
+  let index = 0;
+  let keys = Object.keys(choices);
+  // Adjust probabilities based on temperature
+  let adjustedChoices = {};
+  let sum = 0;
+  for (let key of keys) {
+    // Raise each probability to the power of (1 / temperature)
+    // A higher temperature (> 1) will make the probabilities more uniform (more random)
+    // A lower temperature (< 1) will amplify the differences, making the selection more deterministic
+    adjustedChoices[key] = Math.pow(choices[key], 1 / temperature);
+    sum += adjustedChoices[key];
+  }
+  // Normalize the adjusted probabilities
+  for (let key of keys) {
+    // Normalize the adjusted probabilities so they sum to 1
+    adjustedChoices[key] /= sum;
+  }
+  let start = random(1);
+  while (start > 0) {
+    // Get the next possible choice and subtract its probability from 'start'
+    let nextChoice = keys[index];
+    let probability = adjustedChoices[nextChoice];
+    start -= probability;
+    index++;
+  }
+  index--;
+  return keys[index];
 }
 
 // A Markov Generator class
@@ -18,10 +53,17 @@ export class MarkovGenerator {
     // What is the maximum amount we will generate?
     this.max = max;
     // An object as dictionary
-    // each ngram is the key, a list of possible next elements are the values
+    // each ngram is the key, a dictionary of possible next elements and their counts
     this.ngrams = {};
     // A separate array of possible beginnings to generated text
     this.beginnings = [];
+  }
+
+  fromJSON(data) {
+    this.n = data.n;
+    this.max = data.max;
+    this.ngrams = data.ngrams;
+    this.beginnings = data.beginnings;
   }
 
   // A function to feed in text to the markov chain
@@ -41,27 +83,42 @@ export class MarkovGenerator {
       let next = text.charAt(i + this.n);
       // Is this a new one?
       if (!this.ngrams.hasOwnProperty(gram)) {
-        this.ngrams[gram] = [];
+        this.ngrams[gram] = {};
       }
-      // Add to the list
-      this.ngrams[gram].push(next);
+      // Increment the count for this next character
+      if (!this.ngrams[gram].hasOwnProperty(next)) {
+        this.ngrams[gram][next] = 0;
+      }
+      this.ngrams[gram][next]++;
+    }
+
+    // Normalize the counts to probabilities
+    for (let gram in this.ngrams) {
+      let total = 0;
+      for (let next in this.ngrams[gram]) {
+        total += this.ngrams[gram][next];
+      }
+      for (let next in this.ngrams[gram]) {
+        this.ngrams[gram][next] /= total;
+      }
     }
   }
 
   // Generate a text from the information ngrams
-  generate() {
-    // Get a random  beginning
+  generate(temperature) {
+    // Get a random beginning
     let current = random(this.beginnings);
+
     let output = current;
 
     // Generate a new token max number of times
     for (let i = 0; i < this.max; i++) {
       // If this is a valid ngram
       if (this.ngrams.hasOwnProperty(current)) {
-        // What are all the possible next tokens
+        // What are all the possible next tokens and their probabilities
         let possible_next = this.ngrams[current];
-        // Pick one randomly
-        let next = random(possible_next);
+        // Pick one using weighted random selection
+        let next = weightedSelection(possible_next, temperature);
         // Add to the output
         output += next;
         // Get the last N entries of the output; we'll use this to look up
